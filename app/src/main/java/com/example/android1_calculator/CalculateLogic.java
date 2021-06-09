561d9e411b481638f3e8fa499170de8f1fbfd8d1
Nikolai <nik281288@mail.ru>
2021-06-10 00:33:08 +0300

    Finish CalculateLogic class and set up intent-filter for launch app.

--- app/src/main/java/com/example/android1_calculator/CalculateLogic.java
+++ app/src/main/java/com/example/android1_calculator/CalculateLogic.java
@@ -1,6 +1,145 @@
 package com.example.android1_calculator;
 
 public class CalculateLogic {
+    private int firstArg;
+    private int secondArg;
+    private int actionSelected;
 
+    private final StringBuilder inputStr = new StringBuilder();
 
+    private State state;
+
+    private enum State {
+        firstArgInput,
+        operationSelected,
+        secondArgInput,
+        resultShow
+    }
+
+    public CalculateLogic() {
+        state = State.firstArgInput;
+    }
+
+    public void onNumPressed(int buttonId) {
+
+        if (state == State.resultShow) {
+            state = State.firstArgInput;
+            inputStr.setLength(0);
+        }
+
+        if (state == State.operationSelected) {
+            state = State.secondArgInput;
+            inputStr.setLength(0);
+        }
+
+        if (inputStr.length() < 9) {
+            switch (buttonId) {
+                case R.id.butt0:
+                    if (inputStr.length() != 0) {
+                        inputStr.append("0");
+                    }
+                    break;
+                case R.id.butt1:
+                    inputStr.append("1");
+                    break;
+                case R.id.butt2:
+                    inputStr.append("2");
+                    break;
+                case R.id.butt3:
+                    inputStr.append("3");
+                    break;
+                case R.id.butt4:
+                    inputStr.append("4");
+                    break;
+                case R.id.butt5:
+                    inputStr.append("5");
+                    break;
+                case R.id.butt6:
+                    inputStr.append("6");
+                    break;
+                case R.id.butt7:
+                    inputStr.append("7");
+                    break;
+                case R.id.butt8:
+                    inputStr.append("8");
+                    break;
+                case R.id.butt9:
+                    inputStr.append("9");
+                    break;
+            }
+        }
+    }
+
+    public void onActionPressed(int actionId) {
+        if (actionId == R.id.equals && state == State.secondArgInput && inputStr.length() > 0) {
+            secondArg = Integer.parseInt(inputStr.toString());
+            state = State.resultShow;
+            inputStr.setLength(0);
+            switch (actionSelected) {
+                case R.id.plus:
+                    inputStr.append(firstArg + secondArg);
+                    break;
+                case R.id.minus:
+                    inputStr.append(firstArg - secondArg);
+                    break;
+                case R.id.multiply:
+                    inputStr.append(firstArg * secondArg);
+                    break;
+                case R.id.division:
+                    inputStr.append(firstArg / secondArg);
+                    break;
+            }
+
+        } else if (inputStr.length() > 0 && state == State.firstArgInput && actionId != R.id.equals) {
+            firstArg = Integer.parseInt(inputStr.toString());
+            state = State.operationSelected;
+            actionSelected = actionId;
+        }
+    }
+
+    public String getText() {
+        StringBuilder str = new StringBuilder();
+        switch (state) {
+            default:
+                return inputStr.toString();
+            case operationSelected:
+                return str.append(firstArg).append(' ')
+                        .append(getOperationChar())
+                        .toString();
+            case secondArgInput:
+                return str.append(firstArg).append(' ')
+                        .append(getOperationChar())
+                        .append(' ')
+                        .append(inputStr)
+                        .toString();
+            case resultShow:
+                return str.append(firstArg).append(' ')
+                        .append(getOperationChar())
+                        .append(' ')
+                        .append(secondArg)
+                        .append(" = ")
+                        .append(inputStr.toString())
+                        .toString();
+        }
+    }
+
+    private char getOperationChar() {
+        switch (actionSelected) {
+            case R.id.plus:
+                return '+';
+            case R.id.minus:
+                return '-';
+            case R.id.multiply:
+                return '*';
+            case R.id.division:
+            default:
+                return '/';
+
+        }
+    }
+
+    public void reset() {
+        state = State.firstArgInput;
+        inputStr.setLength(0);
+    }
 }
--- app/src/main/java/com/example/android1_calculator/MainActivity.java
+++ app/src/main/java/com/example/android1_calculator/MainActivity.java
@@ -4,14 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView text;
-    private final int[] numberButtonIds = new int[]{R.id.butt0, R.id.butt1, R.id.butt2, R.id.butt3,
-            R.id.butt4, R.id.butt5, R.id.butt6, R.id.butt7, R.id.butt8, R.id.butt9};
 
+
+    private CalculateLogic calculator;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,73 +19,49 @@ public class MainActivity extends AppCompatActivity {
 
         text = findViewById(R.id.text);
 
-        Button[] buttons = null;
-        buttons[0] = findViewById(numberButtonIds[0]);
-        buttons[1] = findViewById(numberButtonIds[1]);
-        buttons[2] = findViewById(numberButtonIds[2]);
-        buttons[3] = findViewById(numberButtonIds[3]);
-        buttons[4] = findViewById(numberButtonIds[4]);
-        buttons[5] = findViewById(numberButtonIds[5]);
-        buttons[6] = findViewById(numberButtonIds[6]);
-        buttons[7] = findViewById(numberButtonIds[7]);
-        buttons[8] = findViewById(numberButtonIds[8]);
-        buttons[9] = findViewById(numberButtonIds[9]);
-
-        View.OnClickListener OnClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.butt1:
-                        text.setText("1");
-                        break;
-                    case R.id.butt2:
-                        text.setText("2");
-                        break;
-                    case R.id.butt3:
-                        text.setText("3");
-                        break;
-                    case R.id.butt4:
-                        text.setText("4");
-                        break;
-                    case R.id.butt5:
-                        text.setText("5");
-                        break;
-                    case R.id.butt6:
-                        text.setText("6");
-                        break;
-                    case R.id.butt7:
-                        text.setText("7");
-                        break;
-                    case R.id.butt8:
-                        text.setText("8");
-                        break;
-                    case R.id.butt9:
-                        text.setText("9");
-                        break;
-                    case R.id.butt0:
-                        text.setText("0");
-                        break;
-                }
-            }
+        int[] numberIds = new int[]{
+                R.id.butt0,
+                R.id.butt1,
+                R.id.butt2,
+                R.id.butt3,
+                R.id.butt4,
+                R.id.butt5,
+                R.id.butt6,
+                R.id.butt7,
+                R.id.butt8,
+                R.id.butt9,
         };
 
-        for (int id = 0; id < buttons.length; id++) {
-            buttons[id].setOnClickListener(OnClickListener);
+        int[] actionsIds = new int[]{
+                R.id.plus,
+                R.id.minus,
+                R.id.multiply,
+                R.id.division,
+                R.id.equals
+        };
+
+        calculator = new CalculateLogic();
+
+        View.OnClickListener numberButtonClickListener = view -> {
+            calculator.onNumPressed(view.getId());
+            text.setText(calculator.getText());
+        };
+
+        View.OnClickListener actionButtonOnclickListener = view -> {
+            calculator.onActionPressed(view.getId());
+            text.setText(calculator.getText());
+        };
+        for (int numberId : numberIds) {
+            findViewById(numberId).setOnClickListener(numberButtonClickListener);
         }
-// Хотел эту простыню упаковать в цикл, но уже все глаза сломал не понимаю как это сделать!!
-// сам массив не могу обьявить перед методом OnCreate.
 
+        for (int actionsId : actionsIds) {
+            findViewById(actionsId).setOnClickListener(actionButtonOnclickListener);
+        }
 
-//        butt1.setOnClickListener(OnClickListener);
-//        butt2.setOnClickListener(OnClickListener);
-//        butt3.setOnClickListener(OnClickListener);
-//        butt4.setOnClickListener(OnClickListener);
-//        butt5.setOnClickListener(OnClickListener);
-//        butt6.setOnClickListener(OnClickListener);
-//        butt7.setOnClickListener(OnClickListener);
-//        butt8.setOnClickListener(OnClickListener);
-//        butt9.setOnClickListener(OnClickListener);
-//        butt0.setOnClickListener(OnClickListener);
+        findViewById(R.id.butt_C).setOnClickListener(view -> {
+            calculator.reset();
+            text.setText(calculator.getText());
+        });
     }
-
 }
